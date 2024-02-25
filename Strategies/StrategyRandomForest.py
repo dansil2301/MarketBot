46f7983c74e393db5c1a8cdd05fc1f46aebb4c13
Danila Solovenko <dansil2301@gmail.com>
2024-02-25 19:42:26 +0100

    added random forest classifier

--- Strategies/StrategyRandomForest.py
+++ Strategies/StrategyRandomForest.py
@@ -1,7 +1,11 @@
-from datetime import timedelta
+import asyncio
+from datetime import timedelta, datetime
 
+import pandas as pd
 from joblib import load
+from sklearn.preprocessing import StandardScaler
 from tinkoff.invest.grpc.marketdata_pb2 import Candle, CandleInterval
+from tinkoff.invest.utils import quotation_to_decimal
 
 from Strategies.StrategyABS import Strategy
 from Strategies.StrategyAroon import StrategyAroon
@@ -16,32 +20,81 @@ from Strategies.Utils.ActionEnum import ActionEnum
 from historyData.HistoryData import HistoryData
 
 
-class StrategyRandomForest:
+class StrategyRandomForest(Strategy):
     def __init__(self, interval: CandleInterval = CandleInterval.CANDLE_INTERVAL_1_MIN):
-        self.interval = interval
-        self.history_candles_length = 30
-
-        self.param_container = dict()
-
+        super().__init__(interval)
+        self.model = load("../DataAnalyze/random_forest_model.joblib")
         self.strategies = [StrategyEMA(), StrategyRSI(), StrategyMACD(),
                            StrategyBB(), StrategyST(), StrategyStochRSI(),
                            StrategyAroon(), StrategyOBV()]
-        # asyncio.run(self._initialize_moving_avg_container())
 
-    async def _initialize_param_container(self) -> None:
-        period = timedelta(days=self.history_candles_length)
+        self.interval = interval
+        self.history_candles_length = max(strategy.history_candles_length for strategy in self.strategies)
+
+        self.data_for_ss = pd.DataFrame
+        self.standard_scaler = StandardScaler()
+
+        self.param_container = dict()
+        asyncio.run(self._initialize_moving_avg_container())
+
+    async def _initialize_moving_avg_container(self, data_set=None) -> None:
+        period = timedelta(days=20)
         candles = await HistoryData().get_tinkoff_server_data_from_now(period=period, interval=self.interval)
 
-        pass
+        self.param_container = {"close": [], "volume": [], "open": [], "high": [], "low": []}
+        for candle in candles:
+            self._init_standard_candle_params(candle)
+
+        for strategy in self.strategies:
+            strategy.initialize_moving_avg_container(candles[:strategy.history_candles_length])
+            for candle_index in range(strategy.history_candles_length, len(candles)):
+                params = strategy.get_candle_param(candles[candle_index])
+                for param_name in params:
+                    if candle_index == strategy.history_candles_length:
+                        self.param_container[param_name] = list()
+                        [self.param_container[param_name].append(None) for i in
+                         range(strategy.history_candles_length)]
+                    self.param_container[param_name].append(params[param_name])
+
+        self.data_for_ss = pd.DataFrame(self.param_container)
+        self.data_for_ss = self.data_for_ss.drop(self.data_for_ss.index[:self.history_candles_length])
+        self.standard_scaler.fit(self.data_for_ss)
+        self.data_for_ss, self.param_container = pd.DataFrame, dict()
 
     def initialize_moving_avg_container(self, candles: list) -> None:
         pass
 
-    def _param_calculation(self, new_candle: Candle) -> list[float]:
-        pass
+    def _init_standard_candle_params(self, candle: Candle):
+        self.param_container["close"].append(float(quotation_to_decimal(candle.close)))
+        self.param_container["open"].append(float(quotation_to_decimal(candle.open)))
+        self.param_container["high"].append(float(quotation_to_decimal(candle.high)))
+        self.param_container["low"].append(float(quotation_to_decimal(candle.low)))
+        self.param_container["volume"].append(candle.volume)
+
+    def _param_calculation(self, new_candle: Candle) -> int:
+        self.param_container = {"close": [], "volume": [], "open": [], "high": [], "low": []}
+        self._init_standard_candle_params(new_candle)
+
+        for strategy in self.strategies:
+            params = strategy.get_candle_param(new_candle)
+            for param_name in params:
+                self.param_container[param_name] = list()
+                self.param_container[param_name].append(params[param_name])
+
+        self.data_for_ss = pd.DataFrame(self.param_container)
+        self.standard_scaler.partial_fit(self.data_for_ss)
+        scaled_data = self.standard_scaler.transform(self.data_for_ss)
+
+        prediction = self.model.predict(scaled_data)
+        return prediction[0]
 
     def get_candle_param(self, new_candle: Candle) -> list[float]:
         pass
 
     async def trade_logic(self, new_candle: Candle) -> ActionEnum:
-        pass
+        prediction = self._param_calculation(new_candle)
+
+        if prediction == 1:
+            return ActionEnum.BUY
+        else:
+            return ActionEnum.KEEP
--- historyData/HistoryAppTest.py
+++ historyData/HistoryAppTest.py
@@ -6,12 +6,14 @@ from tinkoff.invest import SubscriptionInterval
 from tinkoff.invest.grpc.marketdata_pb2 import CandleInterval
 from tinkoff.invest.utils import decimal_to_quotation, quotation_to_decimal
 
+from AppLogic.Settings import Settings
 from Strategies.StrategyAroon import StrategyAroon
 from Strategies.StrategyBB import StrategyBB
 from Strategies.StrategyEMA import StrategyEMA
 from Strategies.StrategyMA import StrategyMA
 from Strategies.StrategyMACD import StrategyMACD
 from Strategies.StrategyOBV import StrategyOBV
+from Strategies.StrategyRandomForest import StrategyRandomForest
 from Strategies.StrategyST import StrategyST
 from Strategies.StrategyStochRSI import StrategyStochRSI
 from Strategies.Utils.ActionEnum import ActionEnum
@@ -38,15 +40,19 @@ class HistoryAppTest:
         self.startTime = startTime
         self.endTime = endTime
 
+        self.profit_loss = {"profit": 0, "loss": 0}
+
     async def trade(self) -> None:
         all_candles_period = await self.historyData.get_tinkoff_server_data(
             self.startTime, self.endTime, self.candle_interval)
 
         period_candles = all_candles_period[:self.strategy.history_candles_length]
-        self.strategy.initialize_moving_avg_container(period_candles)
+        if self.strategy.__class__.__name__ != "StrategyRandomForest":
+            self.strategy.initialize_moving_avg_container(period_candles)
         for i in range(self.strategy.history_candles_length, len(all_candles_period)):
             self.action = await self.strategy.trade_logic(all_candles_period[i])
             self.take_action(all_candles_period[i])
+        print(self.profit_loss)
 
     def take_action(self, candle):
         if not candle:
@@ -58,7 +64,13 @@ class HistoryAppTest:
                 self.sum -= self.bought_at * self.commission
         elif self.bought_at:
             current_percent = (float(quotation_to_decimal(candle.close)) / self.bought_at - 1) * 100
-            if current_percent >= 0.5 or current_percent <= -0.1:
+            if current_percent >= Settings().percent_up or current_percent <= -Settings().percent_down:
+
+                if current_percent >= Settings().percent_up:
+                    self.profit_loss["profit"] += 1
+                else:
+                    self.profit_loss["loss"] +=1
+
                 if self.bought_at != 0.1 and self.bought_at:
                     current_sum = float(quotation_to_decimal(candle.close))
                     percent = (current_sum / self.bought_at) - 1
@@ -70,10 +82,14 @@ class HistoryAppTest:
 
 if __name__ == "__main__":
     candle_interval = CandleInterval.CANDLE_INTERVAL_1_MIN
-    # date_start = datetime(2022, 2, 1)
-    # end_date = datetime(2022, 3, 1)
-    date_start = datetime(2023, 2, 1)
-    end_date = datetime(2023, 3, 1)
+    date_start = datetime(2022, 2, 1)
+    end_date = datetime(2022, 3, 1)
 
-    test = HistoryAppTest(StrategyOBV(candle_interval), candle_interval, date_start, end_date)
+    # date_start = datetime(2023, 2, 1)
+    # end_date = datetime(2023, 3, 1)
+
+    # date_start = datetime(2024, 1, 1)
+    # end_date = datetime(2024, 2, 1)
+
+    test = HistoryAppTest(StrategyRandomForest(candle_interval), candle_interval, date_start, end_date)
     asyncio.run(test.trade())
